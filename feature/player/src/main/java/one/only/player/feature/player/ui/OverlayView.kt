4ccfab71b15c39d7ce52bc2c4ca469bcb24e2013
qznet <qznet@users.noreply.github.com>
2026-08-30 16:48:34 +0800

    fix: add missing remember import in OverlayView

--- feature/player/src/main/java/one/only/player/feature/player/ui/OverlayView.kt
+++ feature/player/src/main/java/one/only/player/feature/player/ui/OverlayView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.tooling.preview.Preview
